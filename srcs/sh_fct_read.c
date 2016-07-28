cb22bbff6d109b0b7833816798524a51048ed049
dolewski <dolewski@student.42.fr>
2016-07-28 15:49:13 +0200

    > toto ls OK ; ls > toto OK ; ls -l > toto OK ; > toto ls -l KO ; ls -l > toto -a KO

--- srcs/sh_fct_read.c
+++ srcs/sh_fct_read.c
@@ -5,19 +5,21 @@
 
 int				tree_traversal(t_node *tree)
 {
-	printf("tree : %s\n", tree->data);
+	printf("tree : %s - %d\n", tree->data, tree->type);
 	if (tree && tree->left != NULL)
 	{
-		printf("left %s\n", tree->left->data);
+		printf("{{%s left %s}}\n", tree->data, tree->left->data);
 		tree_traversal(tree->left);
 	}
 	if (tree && tree->right != NULL)
 	{
-		printf("right %s\n", tree->right->data);
+		printf("{{%s right %s}}\n", tree->data, tree->right->data);
 		tree_traversal(tree->right);
 	}
-	ft_putstr(tree->data);
-	ft_putstr(" --> ");
+//	ft_putstr("[");
+//	ft_putstr(tree->data);
+//	ft_putstr("]");
+//	ft_putstr(" --> ");
 	return (0);
 }
 
