6adb79732e99d45c71f719ca815c04432db2eee9
Anne-Laure VACHER <avacher@student.42.fr>
2016-07-28 15:57:55 +0200

    lexer bug corrigé

--- srcs/sh_lexer2.c
+++ srcs/sh_lexer2.c
@@ -8,11 +8,8 @@
 
 int				in_lexer_2(t_e_list **tmp, int boule)
 {
-	printf("[[%p]]\n", *tmp);
-	printf("inlexer_2\n");
 	while (*tmp && (*tmp)->next)
 	{
-	printf("inlexer_2 while\n");
 		if (ft_strchr("><", ((*tmp)->next->data)[0]))
 			(*tmp)->next->type = RED;
 		else if (boule == 1 && (!ft_strchr(SPECIAL, ((*tmp)->next->data)[0]) &&
@@ -32,27 +29,30 @@ int				in_lexer_2(t_e_list **tmp, int boule)
 		}
 		else if (boule == 0 && ((ft_strchr(SPECIAL, ((*tmp)->data)[0])
 		&& !ft_strchr("><", ((*tmp)->next->data)[0])) || (*tmp)->type == RA))
+		{
+			boule = 1;
 			(*tmp)->next->type = CMD;
+		}
 		*tmp = (*tmp)->next;
 	}
 	return (0);
-	while ((*tmp) != NULL){printf("(((%s -- %d)))\n",(*tmp)->data, (*tmp)->type); (*tmp)=(*tmp)->next;}
 }
 
 int				lexer_2(t_e_list **l_expr)
 {
 	t_e_list		*tmp;
-	char			boule;
+	int				boule;
 
 	tmp = *l_expr;
 	boule = 0;
 	if (tmp && ft_strchr("><", (tmp->data)[0]))
-	{printf("lexer2 if \n");
+	{
+//		printf("lexer2 if \n");
 		tmp->type = RED;
 	}
 	else if (tmp)
 	{
-	printf("lexer_2 else if\n");
+//	printf("lexer_2 else if\n");
 		tmp->type = CMD;
 		boule = 1;
 	}
