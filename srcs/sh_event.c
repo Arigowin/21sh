d4e3461e4a0554dc21a0e1cf5888c9b26c52097e
Anne-Laure VACHER <avacher@student.42.fr>
2016-10-20 16:38:10 +0200

    quote handler

--- srcs/sh_event.c
+++ srcs/sh_event.c
@@ -5,12 +5,21 @@ int				event(int key, t_line *stline, t_history **history)
 {
 	if (DEBUG == 1)
 		printf("------- EVENT ------\n");
-	if (key == RETURN)
+	if (key == RETURN && stline->quote == 0)
 	{
 		if (stline->line && stline->line[0])
 			add_history(history, stline->line);
 		ft_putchar(key);
 		return (1);
+	}
+	else if (key == RETURN && stline->quote != 0)
+	{
+		ft_putchar(key);
+		if (stline->quote == QUOTE)
+			ft_putstr("quote> ");
+		if (stline->quote == DQUOTE)
+			ft_putstr("dquote> ");
+		return (2);
 	}
 	else if (key == BACKSPACE)
 		backspace(stline);
