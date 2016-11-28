e1da4719a15f309ae963c982e5206ac90d5e63f7
Anne-Laure VACHER <avacher@student.42.fr>
2016-11-28 16:59:31 +0100

    fct insert ok, fct backspace GO!!!

--- srcs/sh_modif_line.c
+++ srcs/sh_modif_line.c
@@ -19,7 +19,7 @@ int			fct_backspace(t_line *stline, t_history **history)
 
 	end_line = ft_strsub(stline->line, stline->pos_line, ft_strlen(stline->line));
 	if ((stline->pos_line > 0 && stline->quote != 0 && stline->curs_x > 2)
-	 || (stline->pos_line > 0 && stline->quote == 0))
+			|| (stline->pos_line > 0 && stline->quote == 0))
 	{
 
 		if (stline->curs_x < 0 && stline->curs_y > 0)
@@ -85,42 +85,47 @@ static void			enlarge_line(t_line *stline)
 	free(tmp);
 }
 
+int					insert_char(t_line *stline, char c, char *end_line)
+{
+	int					i;
+
+	i = 0;
+	// ce == shift + d in vim
+	tputs(tgetstr("ce", NULL), 1, my_outc);
+	// save the current cursor position
+	tputs(tgetstr("sc", NULL), 1, my_outc);
+	(stline->line)[stline->pos_line] = c;
+	i = stline->pos_line + 1;
+	while (end_line[i - stline->pos_line - 1] != 0)
+	{
+		(stline->line)[i] = end_line[i - stline->pos_line - 1];
+		i++;
+	}
+	(stline->line)[i] = '\0';
+	ft_putstr(end_line);
+	// restore the last saved cursor position.
+	tputs(tgetstr("rc", NULL), 1, my_outc);
+	return(TRUE);
+}
+
 int					fct_insert(t_line *stline, char c)
 {
 	if (DEBUG_TERMCAPS == 1)
 		printf("------- INSERT ------\n");
-	char				*tmp;
+	char				*end_line;
 	int					i;
 
-	tmp = NULL;
+	end_line = NULL;
 	if (stline->pos_line % BUFF_SIZE == 0 && stline->pos_line + 1 > BUFF_SIZE)
 		enlarge_line(stline);
 	if ((stline->line)[stline->pos_line] == '\0')
 		(stline->line)[stline->pos_line] = c;
-	else
-	{
-		if (!(tmp = ft_strsub(stline->line, stline->pos_line, ft_strlen(stline->line))))
-			return (-1);
-	}
+	else if (!(end_line = ft_strsub(stline->line, stline->pos_line,
+					ft_strlen(stline->line))))
+		return (-1);
 	ft_putchar(c);
-	if (tmp != NULL)
-	{
-		// ce == shift + d in vim
-		tputs(tgetstr("ce", NULL), 1, my_outc);
-		// save the current cursor position
-		tputs(tgetstr("sc", NULL), 1, my_outc);
-		(stline->line)[stline->pos_line] = c;
-		i = stline->pos_line + 1;
-		while (tmp[i - stline->pos_line - 1] != 0)
-		{
-			(stline->line)[i] = tmp[i - stline->pos_line - 1];
-			i++;
-		}
-		(stline->line)[i] = 0;
-		ft_putstr(tmp);
-		// restore the last saved cursor position.
-		tputs(tgetstr("rc", NULL), 1, my_outc);
-	}
+	if (end_line != NULL)
+		insert_char(stline, c, end_line);
 	stline->curs_x++;
 	stline->pos_line++;
 	if (stline->curs_x >= stline->win.ws_col)
@@ -130,7 +135,6 @@ int					fct_insert(t_line *stline, char c)
 		tputs(tgetstr("cr", NULL), 1, my_outc);
 		tputs(tgetstr("do", NULL), 1, my_outc);
 	}
-	if (tmp)
-		free(tmp);
+	ft_strdel(end_line);
 	return (0);
 }
