a92cdb974e0d64e4278b708aafe49e8d46319355
Anne-Laure VACHER <avacher@student.42.fr>
2016-11-28 17:37:33 +0100

    fct backspace OK

--- srcs/sh_modif_line.c
+++ srcs/sh_modif_line.c
@@ -2,57 +2,69 @@
 #include "libft.h"
 #include <term.h>
 
-int			fct_backspace(t_line *stline, t_history **history)
+int					handle_delete_quote(t_line *stline)
+{
+	if (DEBUG_TERMCAPS == 1)
+		printf("------- HANDLE DELETE QUOTE ------\n");
+	int					pos;
+
+	pos = stline->pos_line;
+	if ((stline->line)[pos - 1] == stline->quote)
+		stline->quote = 0;
+	else if (stline->quote == 0 && ((stline->line)[pos - 1] == QUOTE
+			|| (stline->line)[pos - 1] == DQUOTE))
+		stline->quote = (stline->line)[pos - 1];
+	return (TRUE);
+}
+
+int					delete_char(t_line *stline, int i, int *save_pos)
+{
+	if (DEBUG_TERMCAPS == 1)
+		printf("------- DELETE_CHAR ------\n");
+	if (stline->curs_x < 0 && stline->curs_y > 0)
+	{
+		(stline->curs_x)--;
+		(stline->curs_y)--;
+		tputs(tgetstr("up", NULL), 1, my_outc);
+		while (stline->curs_x++ < stline->win.ws_col)
+			tputs(tgetstr("nd", NULL), 1, my_outc);
+		tputs(tgetstr("dc", NULL), 1, my_outc);
+		(stline->line)[--stline->pos_line] = '\0';
+	}
+	else
+	{
+		(*save_pos) = stline->pos_line;
+		i = stline->pos_line;
+		fct_end(stline, NULL);
+		while (i <= stline->pos_line)
+		{
+			tputs(tgetstr("dc", NULL), 1, my_outc);
+			fct_left(stline, NULL);
+			tputs(tgetstr("dc", NULL), 1, my_outc);
+			(stline->line)[stline->pos_line] = '\0';
+		}
+	}
+	return (TRUE);
+}
+int					fct_backspace(t_line *stline, t_history **history)
 {
 	if (DEBUG_TERMCAPS == 1)
 		printf("------- BACKSPACE ------\n");
-	char	*end_line;
-	int		i;
-	int		save_pos;
+	char				*eol;
+	int					i;
+	int					save_pos;
 
 	save_pos = 0;
-	if ((stline->line)[stline->pos_line - 1] == stline->quote)
-		stline->quote = 0;
-	else if (stline->quote == 0 && ((stline->line)[stline->pos_line - 1] == QUOTE ||
-				(stline->line)[stline->pos_line - 1] == DQUOTE))
-		stline->quote = (stline->line)[stline->pos_line - 1];
-
-	end_line = ft_strsub(stline->line, stline->pos_line, ft_strlen(stline->line));
+	handle_delete_quote(stline);
+	eol = ft_strsub(stline->line, stline->pos_line, ft_strlen(stline->line));
 	if ((stline->pos_line > 0 && stline->quote != 0 && stline->curs_x > 2)
 			|| (stline->pos_line > 0 && stline->quote == 0))
 	{
-
-		if (stline->curs_x < 0 && stline->curs_y > 0)
-		{
-			(stline->curs_x)--;
-			(stline->curs_y)--;
-			tputs(tgetstr("up", NULL), 1, my_outc);
-			while (stline->curs_x < stline->win.ws_col)
-			{
-				tputs(tgetstr("nd", NULL), 1, my_outc);
-				(stline->curs_x)++;
-			}
-			tputs(tgetstr("dc", NULL), 1, my_outc);
-			stline->pos_line--;
-			(stline->line)[stline->pos_line] = 0;
-		}
-		else
-		{
-			save_pos = stline->pos_line;
-			i = stline->pos_line;
-			fct_end(stline, history);
-			while (i <= stline->pos_line)
-			{
-				tputs(tgetstr("dc", NULL), 1, my_outc);
-				fct_left(stline, history);
-				tputs(tgetstr("dc", NULL), 1, my_outc);
-				(stline->line)[stline->pos_line] = 0;
-			}
-		}
 		i = 0;
-		while (end_line && end_line[i])
+		delete_char(stline, i, &save_pos);
+		while (eol && eol[i])
 		{
-			fct_insert(stline, end_line[i]);
+			fct_insert(stline, eol[i]);
 			i++;
 		}
 		while (stline->pos_line >= save_pos)
@@ -60,15 +72,15 @@ int			fct_backspace(t_line *stline, t_history **history)
 		if (stline->curs_x == stline->win.ws_col)
 			tputs(tgetstr("nd", NULL), 1, my_outc);
 	}
-	if (end_line)
-		free(end_line);
+	if (eol)
+		free(eol);
 	return (0);
 }
 
 static void			enlarge_line(t_line *stline)
 {
 	if (DEBUG_TERMCAPS == 1)
-		printf("------- TEST ------\n");
+		printf("------- ENLARGE LINE ------\n");
 	char				*tmp;
 	int					i;
 
@@ -87,6 +99,8 @@ static void			enlarge_line(t_line *stline)
 
 int					insert_char(t_line *stline, char c, char *end_line)
 {
+	if (DEBUG_TERMCAPS == 1)
+		printf("------- INSERT CHAR ------\n");
 	int					i;
 
 	i = 0;
@@ -113,7 +127,6 @@ int					fct_insert(t_line *stline, char c)
 	if (DEBUG_TERMCAPS == 1)
 		printf("------- INSERT ------\n");
 	char				*end_line;
-	int					i;
 
 	end_line = NULL;
 	if (stline->pos_line % BUFF_SIZE == 0 && stline->pos_line + 1 > BUFF_SIZE)
@@ -135,6 +148,6 @@ int					fct_insert(t_line *stline, char c)
 		tputs(tgetstr("cr", NULL), 1, my_outc);
 		tputs(tgetstr("do", NULL), 1, my_outc);
 	}
-	ft_strdel(end_line);
+	ft_strdel(&end_line);
 	return (0);
 }
