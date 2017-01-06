9348d0c0350f73a9a8d2162b8738f867cf44c1cf
dolewski <dolewski@student.42.fr>
2017-01-06 19:48:32 +0100

    probleme decalage derniere ligne shell

--- srcs/sh_modif_line.c
+++ srcs/sh_modif_line.c
@@ -101,18 +101,18 @@ static int			enlarge_line(char **str, int *pos)
 	return (TRUE);
 }
 
-int					insert_char(char c, char *end_line, char **str, int *pos)
+int					insert_char(char c, char *end_line, char **str, int *pos, t_line *stline)
 {
 	if (DEBUG_TERMCAPS == 1)
 		printf("------- INSERT CHAR ------\n");
 	int					i;
+	int					nb_line;
 
 	i = 0;
 	// ce == shift + d in vim
 	tputs(tgetstr("ce", NULL), 1, my_outc);
 	// save the current cursor position
-	tputs(tgetstr("sc", NULL), 1, my_outc);
-	(*str)[(*pos)] = c;
+	(*str)[*pos] = c;
 	i = (*pos) + 1;
 	while (end_line[i - (*pos) - 1] != 0)
 	{
@@ -120,6 +120,33 @@ int					insert_char(char c, char *end_line, char **str, int *pos)
 		i++;
 	}
 	(*str)[i] = '\0';
+
+
+	nb_line = (ft_strlen(*str) + PRT_LEN) / stline->win.ws_col;
+
+	if (nb_line && ((ft_strlen(*str) + PRT_LEN) / nb_line) % stline->win.ws_col == 0)
+	{
+		i = nb_line - stline->curs_y;
+		while (i > 0)
+		{
+			tputs(tgetstr("do", NULL), 1, my_outc);
+			i--;
+		}
+
+		i = nb_line - stline->curs_y;
+		while (i > 0)
+		{
+			tputs(tgetstr("up", NULL), 1, my_outc);
+			i--;
+		}
+		i = stline->curs_x;
+		while (i-- > 0)
+			tputs(tgetstr("nd", NULL), 1, my_outc);
+		tputs(tgetstr("nd", NULL), 1, my_outc);
+	}
+
+
+	tputs(tgetstr("sc", NULL), 1, my_outc);
 	ft_putstr(end_line);
 	// restore the last saved cursor position.
 	tputs(tgetstr("rc", NULL), 1, my_outc);
@@ -137,13 +164,13 @@ int					fct_insert(char c, char **str, int *pos, t_line *stline)
 	if (stline->copy.start != -1)
 		return (FALSE);
 	enlarge_line(str, pos);
-	if ((*str)[(*pos)] == '\0')
-		(*str)[(*pos)] = c;
-	else if (!(end_line = ft_strsub(*str, (*pos), ft_strlen(*str))))
+	if ((*str)[*pos] == '\0')
+		(*str)[*pos] = c;
+	else if (!(end_line = ft_strsub(*str, *pos, ft_strlen(*str))))
 		return (ERROR);
 	ft_putchar(c);
 	if (end_line != NULL)
-		insert_char(c, end_line, str, pos);
+		insert_char(c, end_line, str, pos, stline);
 	stline->curs_x++;
 	(*pos)++;
 	if (stline->curs_x >= stline->win.ws_col)
