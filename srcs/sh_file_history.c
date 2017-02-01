ed5ad0720b6ef9507c2f5ba28ddb1819ff343562
dolewski <dolewski@student.42.fr>
2017-02-01 14:40:03 +0100

    fichier historique et multiligne OK

--- srcs/sh_file_history.c
+++ srcs/sh_file_history.c
@@ -3,15 +3,96 @@
 #include "libft.h"
 #include "get_next_line.h"
 
+static int			in_quote(char **buff, char *line)
+{
+	char				*tmp1;
+	char				*tmp2;
+
+	tmp1 = NULL;
+	tmp2 = NULL;
+	if (*buff && line)
+	{
+		if ((tmp1 = ft_strjoin(line, "\n")) == NULL)
+			return (ERROR);
+		if ((tmp2 = ft_strjoin(*buff, tmp1)) == NULL)
+			return (ERROR);
+		ft_strdel(&tmp1);
+		ft_strdel(buff);
+		if ((*buff = ft_strdup(tmp2)) == NULL)
+			return (ERROR);
+		ft_strdel(&tmp2);
+	}
+	else if (line)
+	{
+		if ((*buff = ft_strjoin(line, "\n")) == NULL)
+			return (ERROR);
+	}
+	return (TRUE);
+}
+
+static int			line_manager(char **buff, char *line, int *quote, t_history **history)
+{
+	char				*tmp;
+
+	tmp = NULL;
+	if (line == NULL || quote == NULL)
+		return (ERROR);
+	if (*quote != 0)
+		return (in_quote(buff, line));
+	else
+	{
+		if (*buff)
+		{
+			if ((tmp = ft_strjoin(*buff, line)) == NULL)
+				return (ERROR);
+			ft_strdel(buff);
+			if ((*buff = ft_strdup(tmp)) == NULL)
+				return (ERROR);
+			ft_strdel(&tmp);
+			add_history(history, *buff);
+			ft_strdel(buff);
+		}
+		else
+			add_history(history, line);
+	}
+	return (TRUE);
+}
+
+static int			get_line_in_file(int fd, t_history **history)
+{
+	int					quote;
+	char				*buff;
+	char				*line;
+
+	buff = NULL;
+	quote = 0;
+	line = NULL;
+	while (fd > -1 && get_next_line(fd, &line) > 0)
+	{
+		if (quote != DQUOTE && ft_strncount(line, QUOTE) % 2 != 0)
+			quote = (quote == QUOTE ? 0 : QUOTE);
+		else if (quote != QUOTE && ft_strncount(line, DQUOTE) % 2 != 0)
+			quote = (quote == DQUOTE ? 0 : DQUOTE);
+		if (line_manager(&buff, line, &quote, history) == ERROR)
+			return (ERROR);
+		ft_strdel(&line);
+	}
+	if (line)
+	{
+		if (line_manager(&buff, line, &quote, history) == ERROR)
+			return (ERROR);
+		ft_strdel(&line);
+	}
+	return (TRUE);
+}
+
 int					load_history(t_history **history)
 {
 	char				*home;
 	char				*path;
-	char				*line;
 	int					fd;
 
 	path = NULL;
-	line = NULL;
 	fd = -1;
 	home = get_env("HOME");
 	if (home)
@@ -19,14 +100,9 @@ int					load_history(t_history **history)
 	if (path && (fd = open(path, O_RDONLY | O_CREAT,  S_IRUSR | S_IWUSR)) == ERROR)
 		return (ERROR);
 	ft_strdel(&path);
-	while (fd > -1 && get_next_line(fd, &line) > 0)
-	{
-		if (line)
-			add_history(history, line);
-	}
-	if (line)
-		add_history(history, line);
-	ft_strdel(&line);
+	ft_strdel(&home);
+	if (get_line_in_file(fd, history) == ERROR)
+		return (ERROR);
 	if (fd > 2)
 		close(fd);
 	return (TRUE);
@@ -48,6 +124,7 @@ int					save_history(void)
 	if (path && (fd = open(path,  O_WRONLY | O_CREAT,  S_IRUSR | S_IWUSR)) == ERROR)
 		return (ERROR);
 	ft_strdel(&path);
+	ft_strdel(&home);
 	while (history && history->prev)
 		history = history->prev;
 	while (history && history->line)
