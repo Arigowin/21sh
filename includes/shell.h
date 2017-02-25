5da864c55ca4c55690eebeb46489590da91f834c
Anne-Laure VACHER <avacher@student.42.fr>
2017-02-25 16:40:16 +0100

    lil modif on error

--- includes/shell.h
+++ includes/shell.h
@@ -11,14 +11,14 @@
 #define DEBUG_KEY 0
 #define DEBUG_LEXER 0
 #define DEBUG_PARSER 0
-#define DEBUG_PIPE 0
+#define DEBUG_PIPE 1
 #define DEBUG_RED 0
 #define DEBUG_SAVIOR 0
 #define DEBUG_TERMCAPS 0
 #define DEBUG_TOKEN 0
 #define DEBUG_TREE 0
 #define DEBUG_TREE_CREATION 0
-#define DEBUG_TREE_VERIF 0
+#define DEBUG_TREE_VERIF 1
 #include <stdio.h>
 
 # define HISTORY_FILE_NAME "/.21sh_history"
