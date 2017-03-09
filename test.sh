1d6531650d0567becd4eb1cb05935a1d7ab0428d
dolewski <dolewski@student.42.fr>
2017-03-09 16:37:12 +0100

    add script test

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd file_test
+SRCS=$(ls)
+for FILE in $SRCS
+do
+	OLD_IFS=$IFS
+	IFS=$'\n'
+	echo $FILE
+	echo "make re && valgrind --leak-check=full --leak-resolution=high ./21sh <" $FILE
+	for line in $(cat $FILE)
+	do
+		echo $line
+		echo
+	done
+	IFS=$OLD_IFS
+done
+cd -
