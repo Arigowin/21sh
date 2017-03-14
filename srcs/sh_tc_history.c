aff1b6fa3ebe23d3dfa085321ef05959c0d11957
Anne-Laure VACHER <avacher@student.42.fr>
2017-03-14 13:38:20 +0100

    add d'un petit free dans add_history

--- srcs/sh_tc_history.c
+++ srcs/sh_tc_history.c
@@ -42,6 +42,7 @@ void				add_history(t_history **history, char *line)
 		*history = new;
 	}
 	savior_history(history, TRUE);
+	del_history(&new); // pour limiter les still reachable
 }
 
 void				del_history(t_history **history)
